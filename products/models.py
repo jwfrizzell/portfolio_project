9b58e815dd13e0d963ea7a2e74f7e4f645cd15c2
Joe Frizzell <joefrizzell@Joes-MBP-2.lan1>
2019-01-01 11:00:13 -0500

    Update model field url to a TextField

--- products/models.py
+++ products/models.py
@@ -4,11 +4,11 @@ from datetime import date
 # Create your models here.
 class Product(models.Model):
 	title = models.CharField(max_length=30)
-	url = models.CharField(max_length=500)
+	url = models.TextField()
 	pub_date = models.DateTimeField(auto_now=False, auto_now_add=True)
 	votes_total = models.IntegerField()
-	icon = models.ImageField(upload_to='images')
-	image = models.ImageField(upload_to='images')
+	icon = models.ImageField(upload_to='images/')
+	image = models.ImageField(upload_to='images/')
 	body = models.TextField()
 
 
