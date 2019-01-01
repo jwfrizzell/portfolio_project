f9a383915be66f8d17f5223f5a59361e4121d097
Joe Frizzell <joefrizzell@Joes-MBP-2.lan1>
2019-01-01 10:55:04 -0500

    Created product model

--- products/models.py
+++ products/models.py
@@ -1,5 +1,18 @@
 from django.db import models
+from datetime import date
 
 # Create your models here.
 class Products(models.Model):
-	item = models.CharField(max_length=30)
+	title = models.CharField(max_length=30)
+	url = models.CharField(max_length=500)
+	pub_date = models.DateTimeField(auto_now=False, auto_now_add=True)
+	votes_total = models.IntegerField()
+	icon = models.ImageField(upload_to='images')
+	body = models.TextField()
+
+
+	def pub_date_pretty(self):
+		return self.pub_date.strftime('%b %e %Y')
+
+	def __str__(self):
+		return self.title
