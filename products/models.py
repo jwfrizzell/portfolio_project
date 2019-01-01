364c5bcd13585cef539f69a8afb0f6830e3b1f06
Joe Frizzell <joefrizzell@Joes-MBP-2.lan1>
2019-01-01 10:55:57 -0500

    Renamed model Products to Product

--- products/models.py
+++ products/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from datetime import date
 
 # Create your models here.
-class Products(models.Model):
+class Product(models.Model):
 	title = models.CharField(max_length=30)
 	url = models.CharField(max_length=500)
 	pub_date = models.DateTimeField(auto_now=False, auto_now_add=True)
