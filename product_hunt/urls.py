1e9c3f07fe72d81b05af04ceaf0ab3bf60cd8bc4
Joe Frizzell <joefrizzell@Joes-MBP-2.lan1>
2019-01-03 18:36:55 -0500

    Build product views and routes

--- product_hunt/urls.py
+++ product_hunt/urls.py
@@ -27,3 +27,5 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('products/', include('products.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- products/views.py
+++ products/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Product
 
 # Create your views here.
 def home(request):
-	return render(request,'products/home.html')
+	products = Product.objects
+	return render(request,'products/home.html', {"products": products})
 
 @login_required
 def create(request):
@@ -15,7 +16,7 @@ def create(request):
 				product = Product()
 				product.title = request.POST['title']
 				product.url = request.POST['producturl']
-				if not request.POST['producturl'].startswith('http'):
+				if not request.POST['producturl'].lower().startswith('http'):
 					product.url = "http://{0}".format(request.POST['producturl'])
 				
 				product.body = request.POST['body']
@@ -23,7 +24,7 @@ def create(request):
 				product.icon = request.FILES['icon']
 				product.hunter = request.user
 				product.save()
-				return render(request,'products/create.html')
+				return redirect('/products/{0}'.format(product.id))
 			except Exception as ex:
 				return render(request,'products/create.html', {
 					'error': "Problem creating product! => {0}".format(str(ex))
@@ -32,3 +33,20 @@ def create(request):
 			return render(request,'products/create.html',{"error": "No fields can be left blank"})
 	else:
 		return render(request,'products/create.html')
+
+
+@login_required
+def detail(request,product_id):
+	product = get_object_or_404(Product,pk=product_id)
+	return render(request,'products/detail.html',{"product": product})
+
+@login_required
+def upvote(request,product_id):
+	if request.method == "POST":
+		product = get_object_or_404(Product,pk=product_id)
+		product.votes_total +=1
+		product.save()
+		return redirect('/products/{0}'.format(product.id))
+	else:
+		return render(request,'products/home.html')
+
